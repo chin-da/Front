ab2eeddd9a0eb538e67e7e9ac1ba70473684e55c
seongminn <berry9901@gmail.com>
2023-07-28 01:12:31 +0900

    [RENAME] global style 파일명 변경

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,10 +1,17 @@
 import { globalCss } from '@stitches/react';
+import { Noto_Sans_KR } from 'next/font/google';
 
-export const GlobalStyles = globalCss({
+const notoSansKr = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
+
+export const globalStyles = globalCss({
   '*': {
     boxSizing: 'border-box',
     padding: 0,
     margin: 0,
+    fontFamily: `${notoSansKr.style.fontFamily}`,
   },
 
   'html, body': {
@@ -13,6 +20,8 @@ export const GlobalStyles = globalCss({
   },
 
   body: {
+    height: '100vh',
+
     color: 'rgb(var(--foreground-rgb))',
     background:
       'linear-gradient(to bottom, transparent, rgb(255, 255, 255)) rgb(214, 219, 220)',
