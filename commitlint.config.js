b57feb65873cdb79fcc60a1142120c2d25d678a0
정내혁 <cnh0301@gmail.com>
2023-08-07 16:32:36 +0900

    [ADD] 백엔드에서 사용하는 commit lint rule 적용

---
+++ commitlint.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^\[(\w+)\] (.+)/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
+  plugins: [
+    {
+      rules: {
+        'header-match-team-pattern': parsed => {
+          const { type, subject } = parsed;
+          if (type === null && subject === null) {
+            return [false, "header must be in format '[type] subject'"];
+          }
+          return [true, ''];
+        },
+      },
+    },
+  ],
+  rules: {
+    'type-empty': [2, 'never'],
+    'header-match-team-pattern': [2, 'always'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'REFACTOR',
+        'CHORE',
+        'DOCS',
+        'FEAT',
+        'FIX',
+        'DEL',
+        'MOVE',
+        'RENAME',
+        'MERGE',
+        'STYLE',
+        'INIT',
+        'ADD',
+      ],
+    ],
+    'type-case': [2, 'always', 'upper-case'],
+  },
+};
