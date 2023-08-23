7cbcb810e4ac9d3a729ff0e6c5eb201fa6f9e3c3
seongminn <berry9901@gmail.com>
2023-08-23 16:04:42 +0900

    [CHORE] Icon Props를 Control로 제어

--- src/components/common/Icon/index.stories.tsx
+++ src/components/common/Icon/index.stories.tsx
@@ -1,15 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import iconName from './Icons';
+
 import Icon from '.';
 
 const meta: Meta<typeof Icon> = {
   component: Icon,
+  argTypes: {
+    iconName: {
+      options: Object.keys(iconName),
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Icon>;
 
-export const Kakao: Story = {
+export const IconComponent: Story = {
   args: {
     iconName: 'kakao',
   },
