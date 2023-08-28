abda47e215db924c15d16e87ab7153746afdac2d
seongminn <berry9901@gmail.com>
2023-08-29 01:58:08 +0900

    [CHORE] kakao oauth redirect url을 환경변수로 관리

--- src/constants/kakaoUrl.ts
+++ src/constants/kakaoUrl.ts
@@ -1,5 +1,5 @@
 export const kakaoOAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${
   process.env.KAKAO_API_KEY
 }&response_type=code&redirect_uri=${encodeURI(
-  'http://localhost:3000/signup',
+  `${process.env.KAKAO_REDIRECT_URL}/kakao`,
 )}&prompt=login`;
