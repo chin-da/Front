8bfbd066298880ee2e145ff43d6e9b7310855e6e
seongminn <berry9901@gmail.com>
2023-09-07 23:00:44 +0900

    [CHORE] InputWrapperProps의 타입명을 FormRowProps로 수정

--- src/components/signup/SignupForm/FormRow/type.ts
+++ src/components/signup/SignupForm/FormRow/type.ts
@@ -2,7 +2,7 @@ import { ComponentProps, ReactNode } from 'react';
 
 import { FormRow } from './style';
 
-export interface InputWrapperProps extends ComponentProps<typeof FormRow> {
+export interface FormRowProps extends ComponentProps<typeof FormRow> {
   id: string;
   label?: ReactNode;
   isRequired?: boolean;
