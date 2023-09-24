cc6b48157f89c690983b2cc75111e93a0dcce38f
정내혁 <cnh0301@gmail.com>
2023-09-24 14:42:00 +0900

    [ADD] pretendard 설정 추가

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,12 +1,13 @@
 import { createStitches } from '@stitches/react';
 import { Noto_Sans_KR } from 'next/font/google';
+import 'pretendard/dist/web/static/pretendard.css';
 
 const notoSansKr = Noto_Sans_KR({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
 });
 
-export const { styled, globalCss, getCssText } = createStitches({
+export const { styled, globalCss, getCssText, css } = createStitches({
   theme: {
     fonts: {
       notoSansKr: `${notoSansKr.style.fontFamily}`,
